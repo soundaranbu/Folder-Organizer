0da05478dd970b6adbb81c8a6925ff791cf256b9
Soundar Anbu <soundaranbu@gmail.com>
2020-08-15 20:03:06 +0530

    Done Home Page UI changes, Added Settings Page

--- Folder Organizer/MainWindow.xaml.cs
+++ Folder Organizer/MainWindow.xaml.cs
@@ -14,32 +14,37 @@ namespace Folder_Organizer
         public MainWindow()
         {
             InitializeComponent();
-            selectedFolderPath.Text = System.Environment.ExpandEnvironmentVariables("%userprofile%/downloads/");
-
+            //         selectedFolderPath.Text = System.Environment.ExpandEnvironmentVariables("%userprofile%/downloads/");
+            _NavigationFrame.Navigate(new HomePage());
         }
 
-        private void organizeFolder_Click(object sender, RoutedEventArgs e)
+        //private void organizeFolder_Click(object sender, RoutedEventArgs e)
+        //{
+        //    //            System.Windows.MessageBox.Show(this, "Organizing Folder.. Please Wait...");
+        //    statusMessage.Content = "Organizing Folder.. Please Wait...";
+        //    _folderOrganizer = new FolderOrganizer.Core.FolderOrganizer(selectedFolderPath.Text, DefaultFolderClassification.GetDefaults());
+
+        //    _folderOrganizer.Organize();
+        //    statusMessage.Content = "Awesome! Your folder is now organized.";
+        //    //            System.Windows.MessageBox.Show(this, "Folder Organized");
+
+        //    Process.Start(selectedFolderPath.Text);
+        //}
+
+        //private void selectFolder_Click(object sender, RoutedEventArgs e)
+        //{
+        //    FolderBrowserDialog dlg = new FolderBrowserDialog();
+        //    dlg.ShowNewFolderButton = true;
+        //    DialogResult result = dlg.ShowDialog();
+        //    if (result == System.Windows.Forms.DialogResult.OK)
+        //    {
+        //        selectedFolderPath.Text = dlg.SelectedPath;
+        //    }
+        //}
+
+        private void settingsButton_Click(object sender, RoutedEventArgs e)
         {
-            //            System.Windows.MessageBox.Show(this, "Organizing Folder.. Please Wait...");
-            statusMessage.Content = "Organizing Folder.. Please Wait...";
-            _folderOrganizer = new FolderOrganizer.Core.FolderOrganizer(selectedFolderPath.Text, DefaultFolderClassification.GetDefaults());
-
-            _folderOrganizer.Organize();
-            statusMessage.Content = "Awesome! Your folder is now organized.";
-            //            System.Windows.MessageBox.Show(this, "Folder Organized");
-
-            Process.Start(selectedFolderPath.Text);
-        }
-
-        private void selectFolder_Click(object sender, RoutedEventArgs e)
-        {
-            FolderBrowserDialog dlg = new FolderBrowserDialog();
-            dlg.ShowNewFolderButton = true;
-            DialogResult result = dlg.ShowDialog();
-            if (result == System.Windows.Forms.DialogResult.OK)
-            {
-                selectedFolderPath.Text = dlg.SelectedPath;
-            }
+            _NavigationFrame.Navigate(new SettingsPage());
         }
     }
 }
